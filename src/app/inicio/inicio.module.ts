fffa29026ec684140872c0bd762fe7ea233a19e9
ErikGranda3756 <egranda3756@uta.edu.ec>
2024-02-14 09:58:07 -0500

    Agrego las otras carpetas del proyecto

---
+++ src/app/inicio/inicio.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { InicioRoutingModule } from './inicio-routing.module';
+import { InicioComponent } from './inicio.component';
+import { IonicModule } from '@ionic/angular';
+
+@NgModule({
+  declarations: [InicioComponent],
+  imports: [CommonModule, InicioRoutingModule, IonicModule],
+})
+export class InicioModule {}
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent implements OnInit {
+  screen: any = 'signin';
+  formData: FormGroup;
+  isLoading: boolean = false;
+  constructor(private fb: FormBuilder) {
+    this.formData = this.fb.group({
+      name: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+    });
+  }
+
+  ngOnInit() {}
+
+  change(event: any) {
+    this.screen = event;
+  }
+
+  login() {}
+
+  register() {}
+}
